bea0b4bbdb8c248259bea99a68a47bd4c5b65adb
SeifMohy <seifmohy@gmail.com>
2022-06-17 17:47:37 +0200

    transaction table ui

--- pages/TransactionHistory.tsx
+++ pages/TransactionHistory.tsx
@@ -1,13 +1,90 @@
-import React from 'react'
-import Layout from '../Components/Layout'
+import React from "react";
+import Layout from "../Components/Layout";
 
+const transactions = [
+  {
+    id: "oapfjpaodijfpoaijsdf",
+    date: "30/12/2020",
+    amount: "$3,509.00",
+    description: "Ahmed ",
+    category: "Sales",
+    bank: "Chase",
+  },
+  // More transactions...
+];
 
 const TransactionHistory = () => {
   return (
-      <Layout>
-    <div>TransactionHistory</div>
+    <Layout>
+      <div>
+        <div className="mt-8 flex flex-col">
+          <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-300">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                      >
+                        Date
+                      </th>
+                      <th
+                        scope="col"
+                        className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Amount
+                      </th>
+                      <th
+                        scope="col"
+                        className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Description
+                      </th>
+                      <th
+                        scope="col"
+                        className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Category
+                      </th>
+                      <th
+                        scope="col"
+                        className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Bank
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-200 bg-white">
+                    {transactions.map((transaction) => (
+                      <tr key={transaction.id}>
+                        <td className="whitespace-nowrap py-2 text-sm text-gray-500 sm:pl-6">
+                          {transaction.date}
+                        </td>
+                        <td className="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">
+                          {transaction.amount}
+                        </td>
+                        <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-900">
+                          {transaction.description}
+                        </td>
+                        <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+                          {transaction.category}
+                        </td>
+                        <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+                          {transaction.bank}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default TransactionHistory
+export default TransactionHistory;
